a89c6a7de5ae39722a44fa6746c2652a2de06986
Jerex3 <jeeremanuell@gmail.com>
2021-03-12 17:31:15 -0300

    hotfix when i create a update query for only 1 propierty

--- src/auxFunctions/createUpdateQuery.js
+++ src/auxFunctions/createUpdateQuery.js
@@ -1,31 +1,39 @@
+const e = require("express");
 
 
 const createQuery = (modifyObject) => { // recibe an object, and create an update query of the keys of the object
 
     let queryFirst = `Set (`
     let querySecond = `(`
-    let length = Object.keys(modifyObject).length-1;
+    let length = Object.keys(modifyObject).length - 1;
     let it = 0;
 
-    for (prop in modifyObject){
+    if (length > 1) {
 
-       queryFirst += prop
-       querySecond += `'${modifyObject[prop]}'`
-        if(length == it){ // Si es la ultima propiedad cierro parentesis.
-            queryFirst += `)`
-            querySecond += `)`
+        for (prop in modifyObject) {
+
+            queryFirst += prop
+            querySecond += `'${modifyObject[prop]}'`
+            if (length == it) { // Si es la ultima propiedad cierro parentesis.
+                queryFirst += `)`
+                querySecond += `)`
+            }
+            else {
+                queryFirst += `, ` // Sino agrego un and
+                querySecond += `, `
+            }
+            it++
         }
-        else{
-            queryFirst += `, ` // Sino agrego un and
-            querySecond += `, `
-        }
-        it++
+    }
+    
+    else{
+        return `set ${Object.keys(modifyObject)[0]} = '${modifyObject[Object.keys(modifyObject)[0]]}' ` 
     }
 
     return queryFirst + ' = ' + querySecond;
 
-} 
+}
 
 module.exports = {
-    createUpdateQuery:createQuery
+    createUpdateQuery: createQuery
 }
