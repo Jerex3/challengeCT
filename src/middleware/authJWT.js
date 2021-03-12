daf51a96f7fd512608bf16953a6d5d2855031dc2
Jerex3 <jeeremanuell@gmail.com>
2021-03-12 15:00:05 -0300

    Cheking if decoded useremail exists at AuthJWT, need to be tested

--- src/middleware/authJWT.js
+++ src/middleware/authJWT.js
@@ -1,17 +1,26 @@
 const jwt = require('jsonwebtoken')
 const config = require('../../config')
-
+const connection = require('../dbConnection')
 const verifyToken = (req, res, next) => {
 
-    const token = req.headers['token-access']
+    const token = req.headers['token-access'] // Obtengo el token del request header
 
-    if(!token) res.status(403).json({message:"No token provided"})
+    if(!token) res.status(403).json({message:"No token provided"}) // if the token dosen't exists return 403
 
-    jwt.verify(token, config.SECRET, (err, decoded) => {
+    jwt.verify(token, config.SECRET, async (err, decoded) => {
 
-        if(err) res.status(401).json({message:"not authorized"})
+        if(err) res.status(401).json({message:"not authorized"}) // if exists but is expired return 401
         
-        else next()
+        else {
+            const client = await  connection.getClient()
+            client.connect()
+            const resultSet = client.query(`select 1 from "Esq"."users" where email = '${decoded.email}'`)
+
+            if (!resultSet.rowCount) res.status(401).json({message:"not authorized"}) // if is not expired but the email-user decoded dosen't exists return 401
+
+            else next() // if exists, its not expired and the user-email decoded exists
+
+        }
 
     } )
 
